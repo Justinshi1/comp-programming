76929087892891654db675fcc6b4b42a7f887f03
Justinshi1 <102479322+Justinshi1@users.noreply.github.com>
2023-02-23 21:04:04 -0800

    Update bitstring.cpp

--- cses./bitstring.cpp
+++ cses./bitstring.cpp
@@ -1,1 +1,26 @@
 //problem link https://cses.fi/problemset/result/5533574/
+#include <iostream>
+#include <math.h>
+using namespace std;
+extern "C" long long getPowerbase2(long long n);
+int main(){
+    long long n = 0;
+    cin >> n;
+    cout<<getPowerbase2(n)<<endl;
+    return 0;
+}
+long long getPowerbase2(long long n){
+    long long two =2;
+    long long res = 0;
+  if (n <= 29 ){
+    return pow(2,n);
+  }
+  else{
+   res = fmod(pow(2, 30), 1000000007);
+    for (long long i = 31; i <= n; i++){
+        res = fmod(res*2, 1000000007);
+    }
+    return res;
+  }
+    
+}
